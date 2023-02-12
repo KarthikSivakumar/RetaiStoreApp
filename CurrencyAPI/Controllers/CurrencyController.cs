7eddcb8fa3431f0dd3ce6ecc9e3fb05e7f858097
karthikksivakumar@gmail.com <karthikksivakumar@gmail.com>
2023-02-12 22:44:13 +0530

    Implement Get for Country API and Currency API

--- CurrencyAPI/Controllers/CurrencyController.cs
+++ CurrencyAPI/Controllers/CurrencyController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using CurrencyAPI.Models;
 
 namespace CurrencyAPI.Controllers;
@@ -7,21 +8,32 @@ namespace CurrencyAPI.Controllers;
 [Route("[controller]")]
 public class CurrencyController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<CurrencyController> _logger;
+    private readonly IWebHostEnvironment _hostingEnvironment;
 
-    public CurrencyController(ILogger<CurrencyController> logger)
+    public CurrencyController(ILogger<CurrencyController> logger,IWebHostEnvironment hostEnvironment)
     {
         _logger = logger;
+        _hostingEnvironment = hostEnvironment;
     }
 
     [HttpGet(Name = "GetCurrency")]
     public IEnumerable<Currency> Get()
     {
-        return new List<Currency>();
+        List<Currency> currencies= new List<Currency>();
+        string rootPath = _hostingEnvironment.ContentRootPath;
+        string absolutePath = Path.Combine(rootPath, "Data/Currencies.json");
+        try
+        {
+            if (System.IO.File.Exists(absolutePath))
+            {
+                currencies = JsonConvert.DeserializeObject<List<Currency>>(System.IO.File.ReadAllText(absolutePath));
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Log(LogLevel.Error, ex, null, Array.Empty<object>());
+        }
+        return currencies;
     }
 }
--- CurrencyAPI/Models/Currency.cs
+++ CurrencyAPI/Models/Currency.cs
@@ -5,8 +5,9 @@ namespace CurrencyAPI.Models
     public class Currency
     {
         public int CurrencyID { get; set; }
-        public IEnumerable<int> CountryIDs {get; set;}
+        public string CurrencyCode { get; set; }
         public string CurrencyName { get; set; }
+        public IEnumerable<int> CountryIDs {get; set;}
         
     }
 }

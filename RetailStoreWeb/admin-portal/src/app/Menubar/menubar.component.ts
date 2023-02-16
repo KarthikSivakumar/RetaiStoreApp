3c938c7ab4e1e48206eba009e045101d1c008b30
karthikksivakumar@gmail.com <karthikksivakumar@gmail.com>
2023-02-16 11:52:49 +0530

    Added menubar component with material for admin portal

---
+++ RetailStoreWeb/admin-portal/src/app/Menubar/menubar.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Menu with icons
+ */
+@Component({
+  selector: 'material-menu',
+  templateUrl: 'menubar.html',
+})
+export class MenuBarComponent {}

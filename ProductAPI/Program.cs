aa20fc6da0d182c154931f623929b39288e169b1
karthikksivakumar@gmail.com <karthikksivakumar@gmail.com>
2023-02-16 11:31:15 +0530

    Registered Product Service in Startup for DI

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -1,7 +1,9 @@
+using ProductAPI.Models;
+using ProductAPI.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddTransient<ProductService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
